bac1d1705d2c5c8db49228364dc87ddc71392c1b
Adin Hudson <adinhudson1996@gmail.com>
2020-05-12 17:28:22 +0530

    added some features

--- public/js/app.js
+++ public/js/app.js
@@ -2,22 +2,27 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
+const messageThree = document.querySelector('#message-3')
 
 weatherForm.addEventListener('submit' , (e) => {
     e.preventDefault()
 
     const location = search.value
 
-    messageOne.textContent = 'Loading...'
+    messageOne.style.display = "block"
+    // messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
+    messageThree.textContent = ''
 
     fetch('/weather?address='+location).then((response) => {
     response.json().then((data) => {
         if (data.error) {
-            messageOne.textContent = data.error
+            messageOne.style.display = "none"
+            messageTwo.textContent = data.error
         } else {
-            messageOne.textContent = data.Location
-            messageTwo.textContent = data.Forecast
+            messageOne.style.display = "none"
+            messageTwo.textContent = data.Location
+            messageThree.textContent = data.Forecast
         }
     })
 })
